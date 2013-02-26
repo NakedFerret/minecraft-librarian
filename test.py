337f6bd944fee60a444f366ec2e39a6d50f16752
Gonzalo Andreani <gonzaandreani@gmail.com>
2013-02-25 21:17:10 -0500

    Finished the cleaner scraping algorithm. It's faster too

--- test.py
+++ test.py
@@ -5,6 +5,28 @@ from BeautifulSoup import BeautifulSoup, SoupStrainer
 block_type_header = "<th>Block type</th>"
 item_header = "<th>Item</th>"
 
+def extract_decimal(td):
+    dec = td
+    if td.span:
+        dec = td.span.string
+    else:
+        dec = td.string
+        
+    return dec
+                           
+def extract_name(td):
+    name = td
+    if td.find('sup'):
+        td.find('sup').extract()
+
+    if td.find('a'):
+        name = td.find('a').string
+    else:
+        name = td.string
+
+    return name
+
+
 f = open("data_values.html")
 source = f.read()
 f.close();
@@ -19,25 +41,19 @@ for t in soup:
     if item_header in t.first().renderContents():
         tables.append(t)
 
-for t in tables[:1]:
+for t in tables:
     rows = t.findAll('tr')
     for tr in rows[1:]:
         non_empty_tds = [x for x in tr.contents if str(x).strip()]
-        name = non_empty_tds[3].find('a').string
 
-        dec = non_empty_tds[1]
-        if dec.span:
-            dec = dec.span.string
-        else:
-            dec = dec.string
-
-        print dec, name
+        dec = extract_decimal(non_empty_tds[1])
+        name = extract_name(non_empty_tds[3])
         
-        # Name -- print tr.find('a', title=True)
-        # ['', ' ', '\n', 'first']
-        # [x for x in l if x.strip()]
+        if len(name) < 3:
+            print("-error-") 
 
-
+        print dec,name
+        
 
 print "done!"
 
