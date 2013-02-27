0768c18cb3c2792f45424e6c6e718f8c1c63f529
Gonzalo Andreani <gonzaandreani@gmail.com>
2013-02-27 17:22:07 -0500

    Updated the WikiTableScraper class with the new algorithm

--- TableScraper.py
+++ TableScraper.py
@@ -1,85 +1,74 @@
 #! /usr/bin/env python2
 
 import urllib
-from BeautifulSoup import BeautifulSoup
-from collections import OrderedDict
+from BeautifulSoup import BeautifulSoup, SoupStrainer
 
-BLOCKIDSTITLE = 'Block IDs'
-ITEMSIDSTITLE = 'Item IDs'
 WIKIURL = 'http://www.minecraftwiki.net/wiki/Data_values'
+BLOCK_TYPE_HEADER = "<th>Block type</th>"
+ITEM_HEADER = "<th>Item</th>"
 
 class WikiTableScraper(object):
     
     def __init__(self):
-        self.dataValues = OrderedDict()
-                
-    def populateData(self,table):
+        self.dataValues = dict()
 
-        rows = table.findAll('tr')
-        for tr in rows[1:]:
-            dec = -1
-            name = None
-            list = tr.contents
-
+    @staticmethod
+    def extract_decimal(td):
+        dec = td
+        if td.span:
+            dec = td.span.string
+        else:
+            dec = td.string
             
-            #[2] and [4] contain dec and block type,respectively
-             if(list[2].span == None):
-                dec = list[2].string.strip()
-            else:
-                dec = list[2].span.string.strip()
+        return dec
 
-            sup = list[4].find('sup')`
-            if sup != None:
-                sup.extract()
-                
-            linkText = ''
-            ays = list[4].findAll('a')
-            for ay in ays:
-                linkText = linkText.strip()
-                linkText = linkText + ' ' +  ay.string
-                ay.extract()
-                
-            text = str(list[4])
-            text  = text.lstrip("<td>")
-            text = text.split("<")[0]
-            text = text.strip()
-        
-            if len(text) > 0:
-                text = text + " " + linkText.strip()
-                name = text
-            else:
-                name = linkText
+    @staticmethod
+    def extract_name(td):
+        name = td
+        if td.find('sup'):
+            td.find('sup').extract()
 
-            self.dataValues[str(dec).strip()] = name.strip()
+        if td.find('a'):
+            name = td.find('a').string
+        else:
+            name = td.string
 
+        return name
 
-    def mineTablesAfterTitle(self,title,numOfTables=1):
-        
-        htwos = self.soup.findAll('h3')
-        for h2 in htwos:
-            if h2.renderContents().find(title) != -1:
-                H2 = h2
-                
-        table = H2.findNext('table')
-        self.populateData(table)
+    def getRelevantTables(self, source_html):
+        tables = list()
+        soup =  BeautifulSoup(source_html, parseOnlyThese=SoupStrainer('table'))
 
-        for i in range(numOfTables-1):
-            table = table.findNext('table')
-            self.populateData(table)
+        for t in soup:
+            if BLOCK_TYPE_HEADER in t.first().renderContents():
+                tables.append(t)
+            if ITEM_HEADER in t.first().renderContents():
+                tables.append(t)
 
-
-    def scrapeTables(self):
-        f = urllib.urlopen(WIKIURL)
-        source = f.read()
-        f.close()
-        self.soup = BeautifulSoup(source)
-        self.mineTablesAfterTitle(BLOCKIDSTITLE,3)
-        self.mineTablesAfterTitle(ITEMSIDSTITLE,4)
-        return self.dataValues
+        return tables
 
     def getValues(self):
         return self.dataValues
 
     def reset(self):
         self.dataValues.clear()
+
+    def scrapeTables(self):
+        f = urllib.urlopen(WIKIURL)
+        source_html = f.read()
+        f.close()
+        
+        tables = self.getRelevantTables(source_html)
+
+        for t in tables:
+            rows = t.findAll('tr')
+            for tr in rows[1:]:
+                non_empty_tds = [x for x in tr.contents if str(x).strip()]
+
+                dec = WikiTableScraper.extract_decimal(non_empty_tds[1])
+                name = WikiTableScraper.extract_name(non_empty_tds[3])
+                self.dataValues[name] = dec
+
+        return self.dataValues
+
 
--- test.py
+++ test.py
@@ -1,59 +1,7 @@
 #! /usr/bin/env python2
 
-from BeautifulSoup import BeautifulSoup, SoupStrainer
+from TableScraper import WikiTableScraper
 
-block_type_header = "<th>Block type</th>"
-item_header = "<th>Item</th>"
-
-def extract_decimal(td):
-    dec = td
-    if td.span:
-        dec = td.span.string
-    else:
-        dec = td.string
-        
-    return dec
-                           
-def extract_name(td):
-    name = td
-    if td.find('sup'):
-        td.find('sup').extract()
-
-    if td.find('a'):
-        name = td.find('a').string
-    else:
-        name = td.string
-
-    return name
-
-
-f = open("data_values.html")
-source = f.read()
-f.close();
-
-soup =  BeautifulSoup(source, parseOnlyThese=SoupStrainer('table'))
-
-tables = list()
-
-for t in soup:
-    if block_type_header in t.first().renderContents():
-        tables.append(t)
-    if item_header in t.first().renderContents():
-        tables.append(t)
-
-for t in tables:
-    rows = t.findAll('tr')
-    for tr in rows[1:]:
-        non_empty_tds = [x for x in tr.contents if str(x).strip()]
-
-        dec = extract_decimal(non_empty_tds[1])
-        name = extract_name(non_empty_tds[3])
-        
-        if len(name) < 3:
-            print("-error-") 
-
-        print dec,name
-        
-
-print "done!"
+scraper = WikiTableScraper()
+print scraper.scrapeTables()
 
