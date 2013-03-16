544471bc2a52f513feb80958dc624769eac4078e
Gonzalo Andreani <gonzaandreani@gmail.com>
2013-03-16 15:16:50 -0400

    Using the command 'test.py | diff result.txt -' to test the TableScraper class

--- test.py
+++ test.py
@@ -1,30 +1,10 @@
 #! /usr/bin/env python2
 
 from TableScraper import WikiTableScraper
-import difflib
-import sys
-import os
-import pickle
-
-CACHE_FILE_NAME = "data_values.cache"
-
-# pathToFile = os.path.join(os.getcwd(), CACHE_FILE_NAME)
-# if os.path.isfile(pathToFile):
-#     # we have a cache
-#     ids = pickle.load( open( CACHE_FILE_NAME, "rb" ) )
-# else:
-#     scraper = WikiTableScraper()
-#     ids = scraper.scrapeTables()
-#     pickle.dump( ids, open( CACHE_FILE_NAME, "wb" ) )
-
-# for i in ids.iteritems():
-#     print i[1].ljust(4), i[0]
 
 scraper = WikiTableScraper()
 ids = scraper.scrapeTables()
 
-import operator
-
-for k,v in sorted(ids.iteritems(), key=operator.itemgetter(1)):
-    print v.ljust(4), k
+for k,v in sorted(ids.iteritems()):
+    print k.ljust(4), v
 
