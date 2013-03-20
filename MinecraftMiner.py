0cdd2c5fa4db2b0d2b0b37033fc14ce7ebb1fa5c
Gonzalo Andreani <gonzaandreani@gmail.com>
2013-03-19 21:42:37 -0400

    Rewrote the Controller class. Argument flow control is now handled by argparse and the main function

--- MinecraftMiner.py
+++ MinecraftMiner.py
@@ -1,151 +1,73 @@
 #! /usr/bin/env python2
 
 import os
-import sys
 from TableScraper import WikiTableScraper
-from collections import OrderedDict
+import argparse
+import pickle
 
-WORKDIRPATH = '.minecraft-miner'
-NAMEOFFILE = 'List-Of-Ids'
+CACHE_DIR = "minecraft-miner"
+FILE_NAME = 'minecraft_objects.cache'
+
+DESCRIPTION = """Search for minecraft objects ids by name. Gets information from Minecraft wiki. First run scrapes the wiki and subsequent runs refer to the cache."""
+POSITIONAL_HELP = """The search words"""
+SEARCH_HELP = """Search for words that contain any of the search words. The default is to search for words that contain ALL the search words"""
+ID_HELP = """Only output the ids of the minecraft objects"""
+UPDATE_HELP = """Force the program to scrape from the wiki instead of using the cache"""
 
 class Controller(object):
 
-    def fillFromFile(self):
-        f = open(NAMEOFFILE,"r")
-        line = f.readline()
-        dec = -1
-        while len(line) > 0:
-            if dec == -1:
-                dec = line.strip()
-            else:
-                self.ids[dec] = line.strip()
-                dec = -1
-            line = f.readline()
-        f.close()
-                
-    def saveToFile(self):
-        f = open(NAMEOFFILE,'w')
-        fileContents = ""
-        for k in self.ids:
-            fileContents += "{0}\n{1}\n".format(k,self.ids[k])
-        f.write(fileContents)
-        f.close()
+    def get_cache_file_path(self):
+        home_path = os.path.expanduser("~")
+        full_cache_dir =  os.path.join(home_path, ".cache", CACHE_DIR)
 
-    def showHelp(self):
-        pass
-
-    def fillData(self,forceUpdate = False):
-        self.ids.clear()
-        #Check to see if the file is there
-        pathToFile = os.path.join(os.getcwd(), NAMEOFFILE)
-        if not forceUpdate and os.path.isfile(pathToFile):
-            self.fillFromFile()
-        else:
-            scraper = WikiTableScraper()
-            self.ids = scraper.scrapeTables()            
-            self.saveToFile()
-
-    def matchWholeWords(self,words):
-        #Match whole words, Ignore uppercase
-        wordKeyList = []
-        for arg in words:
-            for k in self.ids:
-                for word in self.ids[k].split():
-                    if(word.lower() == arg.lower()):
-                        wordKeyList.append(k)
-
-        singleMatches = {}
-        for k in wordKeyList:
-            singleMatches[k] = singleMatches.get(k,0) + 1
-
-        result = []
-        for m in singleMatches:
-            if(singleMatches[m] >= len(words)):
-                result.append(m)
-
-        return result
-                
-    def findClosestMatch(self,words):
-        matchedWords = self.matchWholeWords(words)
+        if not os.path.exists( full_cache_dir ):
+            os.makedirs( full_cache_dir )
         
-        #Make a new dic with [dec : length of all words combined]
-        lenOfMatchedWords = OrderedDict()
-        for d in matchedWords:
-            length = 0
-            for w in self.ids[d].split():
-                length = length + len(w)
-            lenOfMatchedWords[d] = length
-       
-        #Length of all the args combined
-        lenOfArgs = 0
-        for w in words:
-            lenOfArgs += len(w)
-       
-        #Now find the best match i.e. one with closest length
-        bestMatch = matchedWords[0]
-        for i in range(len(lenOfMatchedWords)):
-            bestDiff = lenOfArgs - lenOfMatchedWords[bestMatch]
-            newDiff = lenOfArgs - lenOfMatchedWords[matchedWords[i]]
-            if(abs(newDiff) < abs(bestDiff)):
-                bestMatch = matchedWords[i]
+        return os.path.join( full_cache_dir, FILE_NAME )
 
-        return [bestMatch]
-    
-    def contains(self,words):
-        wordKeyList = []
-        #Compare everyword with everyword in every
-        #Dic entry
-        for arg in words:
-            for k in self.ids:
-                for word in self.ids[k].split():
-                    if(arg.lower() in word.lower()):
-                        wordKeyList.append(k)
+    def search_all(self, word_list):
+        return [k for k,v in self.ids.iteritems() 
+                if all(w.lower() in v.lower() for w in word_list)]
 
-        return wordKeyList
+    def search_any(self, word_list):
+        return [k for k,v in self.ids.iteritems() 
+                if any(w.lower() in v.lower() for w in word_list)]
 
-    def handleArgs(self):
-        args = sys.argv
-        if args[1] == '-h' or args[1] == '--help':
-            self.showHelp()
-            return 0
-        if args[1][:1] == "-":
-            options = args[1][1:]
-            f = self.matchWholeWords
-            
-            if 'w' in options:
-                f = self.matchWholeWords
-            elif 'm' in options:
-                f = self.findClosestMatch
-            elif 'c' in options:
-                f = self.contains
+    def main(self):
+        args = self.parser.parse_args()
+        funct = getattr(self, "search_" + args.search_funct)
+        cache_file_path = self.get_cache_file_path()
 
-            if 'u' in options:
-                self.fillData(True)
-            else:
-                self.fillData()
-
-            result = f(args[2:])
-            for r in result:
-                if('o' in options):
-                    print(r)
-                else:
-                    print('{0} : {1}'.format(r,self.ids[r]))
-            
+        if not os.path.isfile( cache_file_path ) or args.update:
+            self.ids = WikiTableScraper().scrapeTables()
+            pickle.dump( self.ids, open( cache_file_path, "wb" ) )
         else:
-            self.fillData()            
-            result = self.matchWholeWords(args[1:])
-            for r in result:
-                print('{0} : {1}'.format(r,self.ids[r]))
-            
+            self.ids = pickle.load( open( cache_file_path, "rb" ) )
+
+        for k in funct(args.words):
+            print k.ljust(4),
+
+            if not args.only_id:
+                print self.ids[k],
+
+            print 
+                        
+    def setupParser(self):
+        parser = argparse.ArgumentParser(description=DESCRIPTION)
+        parser.add_argument('words', metavar='W', nargs='+', help=POSITIONAL_HELP)
+        parser.add_argument('-a','--any', dest="search_funct", action="store_const", 
+                            const='any', default='all', help=SEARCH_HELP)
+        parser.add_argument('-o','--only-id', action='store_true', help=ID_HELP)
+        parser.add_argument('-u', '--update', action='store_true', help=UPDATE_HELP)
+        return parser
 
     def __init__(self):
-        self.args = sys.argv[1:]
-        self.ids = {}
-        
+        self.ids = dict()
+        self.parser = self.setupParser()
         
 if __name__ == "__main__":
     controller = Controller()
-    controller.handleArgs()
+    controller.main()
 
 
 
