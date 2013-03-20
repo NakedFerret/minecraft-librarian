275d1533a0b9d68327c26b061e30a1efb823be93
Gonzalo Andreani <gonzaandreani@gmail.com>
2013-03-19 23:50:38 -0400

    Reordered functions to follow the flow of the program better. Added comments and docstrings.

--- MinecraftMiner.py
+++ MinecraftMiner.py
@@ -5,9 +5,12 @@ from TableScraper import WikiTableScraper
 import argparse
 import pickle
 
+# the directory where the cache file will be stored
 CACHE_DIR = "minecraft-miner"
+# Name of the cache file
 FILE_NAME = 'minecraft_objects.cache'
 
+# Help texts
 DESCRIPTION = """Search for minecraft objects ids by name. Gets information from Minecraft wiki. First run scrapes the wiki and subsequent runs refer to the cache."""
 POSITIONAL_HELP = """The search words"""
 SEARCH_HELP = """Search for words that contain any of the search words. The default is to search for words that contain ALL the search words"""
@@ -15,9 +18,48 @@ ID_HELP = """Only output the ids of the minecraft objects"""
 UPDATE_HELP = """Force the program to scrape from the wiki instead of using the cache"""
 
 class Controller(object):
+    """This class fetches the minecraft objects from WikiTableScraper, 
+    and searches for item names that match search words. 
+    Uses a cache through the pickle module to speed up subsequent searches.
+    """
+    def __init__(self):
+        """Setup the argument parser"""
+        # Dict of minecraft object in form of "dict[id] = name"
+        self.data_values = dict()
+        self.parser = self.setup_parser()
+
+    def main(self):
+        """Get the objects from the parser (or cache), 
+        search through them, and print the results """
+
+        args = self.parser.parse_args()
+        # Little magic/abomination to avoid a if-else
+        # funct == search_all or search_any
+        funct = getattr(self, "search_" + args.search_funct) 
+        cache_file_path = self.get_cache_file_path()
+
+        # If cache is missing or user asked to force an update
+        # then scrape the wiki
+        if not os.path.isfile( cache_file_path ) or args.update:
+            self.data_values = WikiTableScraper().scrapeTables()
+            pickle.dump( self.data_values, open( cache_file_path, "wb" ) )
+        # else load the minecraft objects from cache
+        else:
+            self.data_values = pickle.load( open( cache_file_path, "rb" ) )
+
+        # call the search function (will return list of ids)
+        for k in funct(args.words):
+            print k.ljust(4), # Don't print a newline
+            # Don't print the name if user requested only id's
+            if not args.only_id:
+                print self.data_values[k],
+            print # print the newline now
 
     def get_cache_file_path(self):
+        """Returns the path of the cache file.
+        Side-Effect: creates ~/.cache or ~/.cache/CACHE_DIR"""
         home_path = os.path.expanduser("~")
+        # path to the programs cache directory
         full_cache_dir =  os.path.join(home_path, ".cache", CACHE_DIR)
 
         if not os.path.exists( full_cache_dir ):
@@ -26,33 +68,20 @@ class Controller(object):
         return os.path.join( full_cache_dir, FILE_NAME )
 
     def search_all(self, word_list):
-        return [k for k,v in self.ids.iteritems() 
+        """Returns list of keys of minecraft objects whose names contain
+        ALL the search words """
+        return [k for k,v in self.data_values.iteritems() 
                 if all(w.lower() in v.lower() for w in word_list)]
 
     def search_any(self, word_list):
-        return [k for k,v in self.ids.iteritems() 
+        """Returns list of keys of minecraft objects whose names contain
+        ANY the search words """
+        # Same as search_all except uses the built-in any()
+        return [k for k,v in self.data_values.iteritems() 
                 if any(w.lower() in v.lower() for w in word_list)]
 
-    def main(self):
-        args = self.parser.parse_args()
-        funct = getattr(self, "search_" + args.search_funct)
-        cache_file_path = self.get_cache_file_path()
-
-        if not os.path.isfile( cache_file_path ) or args.update:
-            self.ids = WikiTableScraper().scrapeTables()
-            pickle.dump( self.ids, open( cache_file_path, "wb" ) )
-        else:
-            self.ids = pickle.load( open( cache_file_path, "rb" ) )
-
-        for k in funct(args.words):
-            print k.ljust(4),
-
-            if not args.only_id:
-                print self.ids[k],
-
-            print 
-                        
-    def setupParser(self):
+    def setup_parser(self):
+        """Returns an argument parser already initialized"""
         parser = argparse.ArgumentParser(description=DESCRIPTION)
         parser.add_argument('words', metavar='W', nargs='+', help=POSITIONAL_HELP)
         parser.add_argument('-a','--any', dest="search_funct", action="store_const", 
@@ -60,10 +89,6 @@ class Controller(object):
         parser.add_argument('-o','--only-id', action='store_true', help=ID_HELP)
         parser.add_argument('-u', '--update', action='store_true', help=UPDATE_HELP)
         return parser
-
-    def __init__(self):
-        self.ids = dict()
-        self.parser = self.setupParser()
         
 if __name__ == "__main__":
     controller = Controller()
